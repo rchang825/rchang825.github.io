b30c73ce5a8acbd0e1cf23c0b1015e2bdfb7dc8e
Ruby Chang <rchang825@hotmail.com>
2025-10-06 16:11:20 -0700

    images and initial styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,13 @@ import projects from './data/projects.js'
 import Card from './components/Card.jsx'
 
 function App() {
-  // useEffect(() => {
-    // You can add your anime.js animations here
-    // For example:
-    // animate({
-    //   targets: '.my-element',
-    //   translateX: 250,
-    //   duration: 2000
-    // })
-  // }, [])
 
   return (
     <div className="app">
       <header className="main">
-        <h1>My Animated Portfolio</h1>
+        <h1>Ruby Chang</h1>
       </header>
+      <div className="card-container">
       {projects.map((project, index) => (
         <Card
           key={index}
@@ -26,11 +18,13 @@ function App() {
           description={project.description}
           image={project.image}
           stack={project.stack}
+          role={project.role}
           link={project.link}
         />
       ))}
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-function Card({ title, image, stack, link }) {
+function Card({ title, image, stack, role, link }) {
   return (
     <div className="card">
       <img src={image} alt={title} />
-      <p>{stack.join(', ')}</p>
       <h2>
         <a href={link} target="_blank">
           {title}
         </a>
       </h2>
+      <p className="role">{role}</p>
+      <p>{stack.join(', ')}</p>
     </div>
   );
 }
