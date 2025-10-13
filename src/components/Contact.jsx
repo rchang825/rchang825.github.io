ae7873907e8f274ab96e25b5aa2c05f66248105e
Ruby Chang <rchang825@hotmail.com>
2025-10-13 16:57:32 -0700

    padding

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaFileAlt } from 'react-icons/fa';
 
 function Contact() {
   return (
     <div className="contact" id="contact">
       <h2>Contact Me</h2>
       <div className="contact-links">
-        <a href="mailto:rchang825@hotmail.com" className="contact-link">
-          <FaEnvelope size={24} />
-          <span>rchang825@hotmail.com</span>
-        </a>
         <a href="https://www.linkedin.com/in/rchang825" target="_blank" rel="noopener noreferrer" className="contact-link">
           <FaLinkedin size={24} />
           <span>linkedin.com/in/rchang825</span>
@@ -17,6 +13,10 @@ function Contact() {
         <a href="https://github.com/rchang825" target="_blank" rel="noopener noreferrer" className="contact-link">
           <FaGithub size={24} />
           <span>github.com/rchang825</span>
+        </a>
+        <a href="./images/resume.pdf" target="_blank" className="contact-link">
+          <FaFileAlt size={24} />
+          <span>Resume</span>
         </a>
       </div>
     </div>
